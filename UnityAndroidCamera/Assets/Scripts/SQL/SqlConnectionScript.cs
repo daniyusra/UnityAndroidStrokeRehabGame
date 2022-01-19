99c5cda6664889d76de3caeb1afba47c727bd88c
Zafira Binta <bintazafira@gmail.com>
2022-01-19 19:40:22 +0700

    Added START MENU

--- UnityAndroidCamera/Assets/Scripts/SQL/SqlConnectionScript.cs
+++ UnityAndroidCamera/Assets/Scripts/SQL/SqlConnectionScript.cs
@@ -1,26 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System.Data.SqlClient;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using System.Data.SqlClient;
 
 
-public class SqlConnectionScript : MonoBehaviour
-{
-    private string connstring;
-    // Start is called before the first frame update
-    void Start()
-    {
-        connstring = "Server=tcp:unityposedetection.database.windows.net,1433;Initial Catalog=unityposedetection;Persist Security Info=False;User ID=daniyusra;Password=ImagineCup2022;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-        SqlConnection conn = new SqlConnection(connstring);
-        conn.Open();
-        SqlCommand cmd = new SqlCommand("Insert Into userData Values ('COK5', 'COK', 'COK', '20120618 10:34:09 AM')", conn);
-        cmd.ExecuteNonQuery();
-        conn.Close();
-    }
+//public class SqlConnectionScript : MonoBehaviour
+//{
+//private string connstring;
+// Start is called before the first frame update
+//void Start()
+//{
+//connstring = "Server=tcp:unityposedetection.database.windows.net,1433;Initial Catalog=unityposedetection;Persist Security Info=False;User ID=daniyusra;Password=ImagineCup2022;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+//SqlConnection conn = new SqlConnection(connstring);
+//conn.Open();
+//SqlCommand cmd = new SqlCommand("Insert Into userData Values ('COK5', 'COK', 'COK', '20120618 10:34:09 AM')", conn);
+//cmd.ExecuteNonQuery();
+//conn.Close();
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
+// Update is called once per frame
+//void Update()
+//{
 
-    }
-}
+//}
+//}
