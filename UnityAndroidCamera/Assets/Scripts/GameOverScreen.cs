1453fc7d6af2b2a25aa35fe5984c06ed4b17cf5b
Zafira Binta <bintazafira@gmail.com>
2022-01-20 16:01:13 +0700

    completed GAME OVER SCREEN restart and menu button

--- UnityAndroidCamera/Assets/Scripts/GameOverScreen.cs
+++ UnityAndroidCamera/Assets/Scripts/GameOverScreen.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameOverScreen : MonoBehaviour
 {
     public Text pointsText;
     public static GameOverScreen inst;
+    
     public void Setup (int score)
-    {
-        
+    {  
         Debug.Log("masuk setup");
         gameObject.SetActive(true);
         pointsText.text = score.ToString() + " POINTS";
+    }
+
+    public void RestartButton()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void ExitButton()
+    {
+        SceneManager.LoadScene("MenuScene");
     }
 
 }
